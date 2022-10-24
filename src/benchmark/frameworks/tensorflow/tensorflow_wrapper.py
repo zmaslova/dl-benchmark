24a20baef312d81b7cad0c3e656fc08f042a358f
Natalya Berezina <n.berezina@yadro.com>
2022-10-24 18:16:19 +0300

    remove commented code

--- src/benchmark/frameworks/tensorflow/tensorflow_wrapper.py
+++ src/benchmark/frameworks/tensorflow/tensorflow_wrapper.py
@@ -13,7 +13,3 @@ class TensorFlowWrapper(FrameworkWrapper):
     @staticmethod
     def create_test(model, dataset, indep_parameters, dep_parameters):
         return TensorFlowTest(model, dataset, indep_parameters, dep_parameters)
-
-    # @staticmethod
-    # def get_dependent_parameters_parser():
-    #     return TensorFlowParametersParser()
