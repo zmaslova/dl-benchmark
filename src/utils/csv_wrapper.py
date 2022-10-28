27c38e23aa60b50fc7bdd555ceebbde8ae4f82d3
Ilya Naumov <i.naumov@yadro.com>
2022-10-28 18:12:00 +0300

    Draft implementation for accuracy_checker

--- src/utils/csv_wrapper.py
+++ src/utils/csv_wrapper.py
@@ -1,33 +1,44 @@
 import csv
 
 
-class CsvReader:
-    def __init__(self, path):
-        self.path = path
+class CsvReport:
+    def __init__(self, path, output_headers=None, output_delimiter=';'):
+        """CsvReport constructor
+
+        :param path: Path to CSV file
+        :param output_headers: List of column headers to use in output file
+        :param output_delimiter: Delimiter to use in output file, defaults to ';'
+        """        
+        self._path = path
+        self._headers = output_headers
+        self._delimiter = output_delimiter
 
     def read(self):
-        with open(self.path, 'r') as csv_file:
+        """Read all data from the CSV file. File format details (like used delimiter) 
+        will be deduced automatically.
+
+        :return: List of dicts with column name and cell value
+        """        
+        with open(self._path, 'r') as csv_file:
             dialect = csv.Sniffer().sniff(csv_file.read(1024))
             csv_file.seek(0)
             reader = csv.DictReader(csv_file, dialect=dialect)
-            return [row for row in reader]
+            return list(reader)
 
-class CsvWriter:
-    def __init__(self, path, headers, delimiter = ';'):
-        self.path = path
-        self.headers = headers
-        self.delimiter = delimiter     
-    
     def write_headers(self):
-        with open(self.path, 'w') as csv_file:
-            self._writer = csv.DictWriter(csv_file, fieldnames=self.headers, dialect=csv.excel, 
-                                          delimiter=self.delimiter, quoting=csv.QUOTE_ALL)
+        """Create CSV file with headers
+        """        
+        with open(self._path, 'w') as csv_file:
+            self._writer = csv.DictWriter(csv_file, fieldnames=self._headers, dialect=csv.excel,
+                                          delimiter=self._delimiter, quoting=csv.QUOTE_ALL)
             self._writer.writeheader()
 
     def append_row(self, row_dict):
-        with open(self.path, 'a') as csv_file:
-            self._writer = csv.DictWriter(csv_file, fieldnames=self.headers, dialect=csv.excel, 
-                                          delimiter=self.delimiter, quoting=csv.QUOTE_ALL)
+        """Append row to the CSV file
+
+        :param row_dict: Dict with column name and cell value
+        """        
+        with open(self._path, 'a') as csv_file:
+            self._writer = csv.DictWriter(csv_file, fieldnames=self._headers, dialect=csv.excel,
+                                          delimiter=self._delimiter, quoting=csv.QUOTE_ALL)
             self._writer.writerow(row_dict)
-
-
